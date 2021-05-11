5944a2cdd6c80e99d333bd5183bc6669534ee971
sholtreman <sebastiaoh27@gmail.com>
2021-05-11 16:03:00 +0200

    more heroku config

--- server/server.js
+++ server/server.js
@@ -24,7 +24,11 @@ app.use(urlencoded({ extended: true }));
 app.use(requireHTTPS)
 app.listen(process.env.PORT || 3000)
 //server.listen(3000);
-app.use(express.static(__dirname + '/public'))
+app.use(express.static('.dist/frontend'))
+
+app.get('/', (req,res)=>{
+  res.sendFile('index.html',{root: 'dist/frontend'})
+})
 
 //room
 app.get('/api/rooms',roomRest);
